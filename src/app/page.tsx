b8e2cd262a85842fc30b0f0aa7d43549ea8afc3a
ygorrfs <ygorrfs@live.com>
2023-10-16 18:05:33 -0300

    finish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ const Page = () => {
   }
 
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-blue-600">
+    <div className="w-full h-screen flex justify-center items-center">
       <div className="w-full max-w-xl rounded-md bg-white text-black shadow shadow-black">
         <div className="p-5 font-bold text-2xl border-b border-gray-300">{title}</div>
         <div className="p-5">
